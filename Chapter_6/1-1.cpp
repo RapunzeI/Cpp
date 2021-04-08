2c278f2f2ace32518d039cea00f99b0d5cfa1031
RapunzeI <71184811+RapunzeI@users.noreply.github.com>
2021-04-08 19:14:39 +0900

    Chapter_6

---
+++ Chapter_6/1-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int add(char c, int a);
+int add(char c, int a, int b);
+
+int add(int a[], int b){
+	int sum = 0;
+	for (int i = 0; i < b; i++){
+		sum += a[i];
+	}
+	return sum;
+}
+int add(int a[], int b, int c[]){
+	int sum = 0;
+	for (int i = 0; i < b; i++){
+		sum += a[i];
+		sum += c[i];
+	}
+	return sum;
+}
+
+int main(){
+	int a[] = { 1, 2, 3, 4, 5 };
+	int b[] = { 6, 7, 8, 9, 10 };
+	int c = add(a, 5);
+	int d = add(a, 5, b);
+	cout << c << endl;
+	cout << d << endl;
+}
