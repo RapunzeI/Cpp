5ad4f37bf33c8bddfc000b05f6b5fbb60a52296b
Alswl_ya <71184811+RapunzeI@users.noreply.github.com>
2021-05-13 19:18:48 +0900

    Chapter_9

---
+++ Chapter_9/3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class LoopAdder{
+	string name;
+	int x, y, sum;
+	void read();
+	void write();
+protected:
+	LoopAdder(string name = ""){
+		this->name = name;
+	}
+	int getX(){ return x; }
+	int getY(){ return y; }
+	virtual int calculate() = 0;
+public:
+	void run();
+};
+
+void LoopAdder::read(){
+	cout << name << ":" << endl;
+	cout << "처음 수에서 두번째 수까지 더합니다. 두 수를 입력하세요 >> ";
+	cin >> x >> y;
+}
+void LoopAdder::write(){
+	cout << x << "에서 " << y << "까지의 합 = " << sum << " 입니다." << endl;
+}
+void LoopAdder::run(){
+	read();
+	sum = calculate();
+	write();
+}
+
+class ForLoopAdder : public LoopAdder{
+    string name;
+public:
+    ForLoopAdder(string s) : LoopAdder(s){
+        name = s;
+    }
+    int calculate(){
+        int sum = 0;
+        int x = getX();
+        int y = getY();
+        for (int i = x; i <= y; i++){
+            sum += i;
+        }
+        return sum;
+    }
+};
+
+int main(){
+    ForLoopAdder forLoop("For Loop");
+    forLoop.run();
+}
